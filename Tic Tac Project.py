b601ae1f0961d2004fe19909a2686fd0b2d9d6a2
Tonny1997 <160511968+Tonny1997@users.noreply.github.com>
2024-07-06 14:04:08 +0300

    Update Tic Tac Project.py

--- Tic Tac Project.py
+++ Tic Tac Project.py
@@ -1,29 +1,31 @@
+#You an also also CMD
 from random import randint
 
 #create a list of play options
 t = ["Rock", "Paper", "Scissors"]
 
 #assign a random play to the computer
+
 computer = t[randint(0,2)]
 
-#set player to False
+#Make the player to False
 player = False
 
 while player == False:
-#set player to True
+#Now set player to True
     player = input("Rock, Paper, Scissors?")
     if player == computer:
-        print("Tie!")
+        print(" This a Tie!")
     elif player == "Rock":
         if computer == "Paper":
-            print("You lose!", computer, "covers", player)
+            print("You have lost!", computer, "covers", player)
         else:
-            print("You win!", player, "smashes", computer)
+            print("You have won!", player, "smashes", computer)
     elif player == "Paper":
         if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
+            print("You have now  lost!", computer, "cut", player)
         else:
-            print("You win!", player, "covers", computer)
+            print("You won!", player, "covers", computer)
     elif player == "Scissors":
         if computer == "Rock":
             print("You lose...", computer, "smashes", player)
